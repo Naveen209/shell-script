5ec66f34c73c1aab01f453e9aa6f1539e91cb65a
Naveen <naveen@example.com>
2026-08-01 00:25:07 +0530

    practice variables

--- arrays.sh
+++ arrays.sh
@@ -1,4 +1,4 @@
 Names=("NAVEEN" "KUMAR" "RAMANA" "SIRI" "PURNA")
-echo "First person name is : {NAMES[0]}"
-echo "Second person name is : {NAMES[1]}"
-echo "All persons names are : {NAMES[@]}"
+echo "First person name is : ${NAMES[0]}"
+echo "Second person name is : ${NAMES[1]}"
+echo "All persons names are : ${NAMES[@]}"
