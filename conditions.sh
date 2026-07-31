8c6d811fe1c142956f0121d7bea0410175aba30a
Naveen <naveen@example.com>
2026-08-01 01:09:28 +0530

    practice variables

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 Userid=$(id -u)
 script_name=$0
-Date=$(date)
-logfile=/tmp/$0-Date.log
+Date=$(date +%F:%H:%M:%S)
+logfile=cat >>/tmp/$0-$Date.log
+
+VALIDATE () {
+if [ $1 -ne 0 ]
+then
+    echo "installation of $1 failure"
+    exit 1
+else
+    echo "installation od $1 success"
+fi
+}
 if [ $Userid -eq 0 ]
 then
     echo "Running as root user starting installation"
@@ -10,10 +20,5 @@ else
     echo "Please run the script as root user"
 fi 
 yum install mysql -y &>>logfile
-if [ $? -ne 0 ]
-then
-    echo "installation failure"
-    exit 1
-else
-    echo "installation success"
-fi
+VALIDATE() $?
+
