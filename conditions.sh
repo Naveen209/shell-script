1966a27c0892e5ea91366090e75f0cfc40c8a6f5
Naveen <naveen@example.com>
2026-08-01 00:48:46 +0530

    practice variables

---
+++ conditions.sh
@@ -0,0 +1,18 @@
+Userid=$(id -u)
+script_name=$0
+Date=$(date)
+logfile=/tmp/$0-Date.log
+if [$Userid -eq 0]
+then
+    echo "Running as root user starting installation"
+else
+    echo "Please run the script as root user"
+fi 
+yum install mysql -y &>>logfile
+if [$? -ne 0]
+then
+    echo "installation failure"
+    exit 1
+else
+    echo "installation success"
+fi
