40b388f1061cda47a7fa2272876bd14b69dfb891
Naveen <naveen@example.com>
2026-08-02 19:32:06 +0530

    practice variables

--- gmail.sh
+++ gmail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 LOGSDIR=/tmp
 DATE=$(date +%F)
-SCRIPT_NAME=$0
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
-LOGFILE=$LOGSDIR/$0-$DATE.log
+SCRIPT_NAME=$(basename "$0" .sh)
+LOGFILE="$LOGSDIR/$SCRIPT_NAME-$DATE.log"
 USERID=$(id -u)
 SASL_FILE="/etc/postfix/sasl_passwd"
 SMTP_SERVER="[smtp.gmail.com]:587"
@@ -23,23 +23,21 @@ VALIDATE() {
 if [ $USERID -ne 0 ]
 then
     echo -e "$R This script execution requires root access, rerunning as root user $N"
-    exec sudo bash "$0" "$@" &>> $LOGFILE
-    VALIDATE $? "root access"
+    exec sudo bash "$0" "$@" &>> "$LOGFILE"
 fi
-yum update -y --exclude=kernel* &>> $LOGFILE
+yum update -y --exclude=kernel* &>> "$LOGFILE"
 VALIDATE $? "Update kernel"
-yum install -y postfix cyrus-sasl cyrus-sasl-plain mailx &>> $LOGFILE
+yum install -y postfix cyrus-sasl cyrus-sasl-plain mailx &>> "$LOGFILE"
 VALIDATE $? "Installing postfix"
-systemctl start postfix &>> $LOGFILE
+systemctl start postfix &>> "$LOGFILE"
 VALIDATE $? "Starting postfix"
-systemctl enable postfix &>> $LOGFILE
+systemctl enable postfix &>> "$LOGFILE"
 VALIDATE $? "enable postfix"
-systemctl status postfix &>> $LOGFILE 
+systemctl status postfix &>> "$LOGFILE"
 VALIDATE $? "enable postfix"
 
-cp postfix.repo /etc/postfix/main.cf &>> $LOGFILE
+cp postfix.repo /etc/postfix/main.cf &>> "$LOGFILE"
 VALIDATE $? "Copying repos"
-mkdir -p "$(dirname "$SASL_FILE")"
 read -s -p "Enter Gmail App Password: " SMTP_PASS
 echo
 VALIDATE $? "Password authenticated"
@@ -48,6 +46,6 @@ echo "${SMTP_SERVER} ${SMTP_USER}:${SMTP_PASS}" > "$SASL_FILE"
 chmod 600 "$SASL_FILE"
 postmap "$SASL_FILE"
 systemctl restart postfix
-systemctl status postfix &>> $LOGFILE
+systemctl status postfix &>> "$LOGFILE"
 echo "This is a test mail & Date $(date)" | mail -s "CentOS 9 Test" "$SMTP_USER"
-
+VALIDATE $? "Sending test mail"
