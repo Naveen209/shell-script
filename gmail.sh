d20be1ccdbe0faf97211ae65d5ea1f2c71d4597c
Naveen <naveen@example.com>
2026-08-02 18:54:42 +0530

    practice variables

---
+++ gmail.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+LOGSDIR=/tmp
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+SASL_FILE="/etc/postfix/sasl_passwd"
+SMTP_SERVER="[smtp.gmail.com]:587"
+SMTP_USER="naveenkumarseelam209@gmail.com"
+SMTP_PASS="Mypass"
+
+VALIDATE() {
+    if [ $? -ne 0 ]
+    then
+        echo -e " $2 $R FAILURE $N "
+    else
+        echo -e " $2 $G SUCCESS $N "
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R This script execution requires root access, rerunning as root user $N"
+    exec sudo bash "$0" "$@" >>& $LOGFILE
+    VALIDATE $? "root access"
+fi
+yum update -y --exclude=kernel* >>& $LOGFILE
+VALIDATE $? "Update kernel"
+yum install -y postfix cyrus-sasl cyrus-sasl-plain mailx &>> $LOGFILE
+VALIDATE $? "Installing postfix"
+systemctl start postfix >>& $LOGFILE
+VALIDATE $? "Starting postfix"
+systemctl enable postfix >>& $LOGFILE
+VALIDATE $? "enable postfix"
+systemctl status postfix >>& $LOGFILE
+VALIDATE $? "enable postfix"
+
+cp postfix.repo /etc/postfix/main.cf &>> $LOGFILE
+VALIDATE $? "Copying repos"
+mkdir -p "$(dirname "$SASL_FILE")"
+echo "${SMTP_SERVER} ${SMTP_USER}:${SMTP_PASS}" > "$SASL_FILE"
+chmod 600 "$SASL_FILE"
+postmap "$SASL_FILE"
+systemctl restart postfix
+systemctl status postfix >>& $LOGFILE
+echo "This is a test mail & Date $(date)" | mail -s "CentOS 9 Test" "$SMTP_USER"
+
